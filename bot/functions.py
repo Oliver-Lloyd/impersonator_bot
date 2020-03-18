6a2ed4cb13a80ee05157051abfdada5685e5aee5
lloyd334 <ollielloyd96@gmail.com>
2020-03-18 15:16:40 +0000

    functions ready to go

--- bot/functions.py
+++ bot/functions.py
@@ -1,4 +1,4 @@
-def load_model(model_path, weights_directory):
+def load_model(model_path='./model.json', weights_directory='.'):
     from tensorflow import keras
     from os import listdir
 
@@ -21,27 +21,60 @@ def load_model(model_path, weights_directory):
     return model
 
 
-def process_x(requested_text, path_to_dict_json):
+def process_x(requested_text, char_to_int_path='char_to_int.json'):
     import json
     import numpy as np
 
     # Load in converter dict
-    with open(path_to_dict_json, "r") as f:
+    with open(char_to_int_path, "r") as f:
         char_to_int = json.load(f)
 
-    # Convert to list
+    # Convert to list (in future may want to handle multiple requests at once)
     if type(requested_text) != list:
         requested_list = [requested_text]
     else:
         requested_list = requested_text
 
-    X = []
+    x = []
     for request in requested_list:
         processed = []
         for char in request:
             value = char_to_int[char]
             processed.append(value)
-        X.append(processed)
+        x.append(np.array(processed))
 
-    X = np.array(X)
-    return X
+    x = np.array(x)
+    x = np.reshape(x, (len(x), len(requested_text), 1))
+    return x
+
+
+def generate(input_text, num_chars, model_path='./model.json', char_to_int_path='./char_to_int.json', int_to_char_path='./int_to_char.json'):
+    # Scratch space below for testing
+    import json
+    with open(char_to_int_path, "r") as f:
+        char_to_int = json.loads(f.read())
+    with open(int_to_char_path, "r") as f:
+        int_to_char = json.loads(f.read())
+    og_text = input_text
+    model = load_model(model_path, '.')
+    input_len = model.input_shape[1]
+    for _ in range(num_chars):
+        if len(og_text) < input_len:
+            text = og_text
+            while len(text) != input_len:
+                text = ' ' + text
+        elif len(og_text) > input_len:
+            text = og_text[-input_len:]
+        else:
+            text = og_text
+
+        x = process_x(text, 'char_to_int.json')
+        predict_probs = model.predict(x)
+        best_prob, best_i = 0, None
+        for i, prob in enumerate(predict_probs[0]):
+            if prob > best_prob:
+                best_prob = prob
+                best_i = i
+        og_text = og_text + int_to_char[str(best_i)]
+
+    return og_text
