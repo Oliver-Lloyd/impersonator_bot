dacab1ad40f098a746eeafe13a3519bb9a15b1c0
lloyd334 <ollielloyd96@gmail.com>
2020-03-20 08:52:50 +0000

    split data processing from training script

--- bot/training.py
+++ bot/training.py
@@ -1,57 +1,12 @@
-"""
-Script that trains a neural network on NL's dialogue
-
-To-do:
-- Hook up to youtube data
-"""
-
-
-import numpy as np
-import json
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.utils import np_utils
+from numpy import load
+
 
 if __name__ == "__main__":
-
-    # Load in transcripts
-    with open('test_transcripts/afterbirthplus_transcripts.txt') as file:
-        raw_text = file.read()
-
-    # Processing. (youtube transcripts dont seem to need much)
-    text = raw_text.replace('\n', ' ')
-
-    # Create and store dicts for converting
-    chars = set(text)
-    char_to_int = dict((c, i) for i, c in enumerate(chars))
-    json_raw = json.dumps(char_to_int)
-    f = open("char_to_int.json", "w")
-    f.write(json_raw)
-    f.close()
-
-    int_to_char = dict((i, c) for i, c in enumerate(chars))
-    json_raw = json.dumps(int_to_char)
-    f = open("int_to_char.json", "w")
-    f.write(json_raw)
-    f.close()
-
-    # Create X and y
-    X_raw = []
-    y_raw = []
-    seq_length = 140
-    for i, char in enumerate(text):
-        #print(i*100/len(text))
-        if i >= seq_length:
-            sub_string = text[i-seq_length:i]
-            x_instance = [char_to_int[character] for character in sub_string]
-            y_instance = char_to_int[char]
-            X_raw.append(np.array(x_instance))
-            y_raw.append(np.array(y_instance))
-
-    X = np.reshape(X_raw, (len(X_raw), seq_length, 1))
-    X = X/len(chars)  # Normalising
-    y = np_utils.to_categorical(y_raw)
+    X = load('NL_X.npy')
+    y = load('NL_y.npy')
 
     # Build model and store architecture as json
     model = Sequential()
